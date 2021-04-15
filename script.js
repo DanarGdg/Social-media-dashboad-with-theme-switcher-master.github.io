f53e395fdb161e5246ee9b7e35530150ac751db8
Danar <>
2021-04-15 13:08:58 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+var toggleText = document.getElementById("toggleText");
+var checkbox = document.getElementById("chk");
+var label = document.getElementById("label");
+var hovercolor = "hsl(0, 0%, 100%)";
+var outcolor = "hsl(228, 34%, 66%)";
+
+checkbox.addEventListener("click", activeToggle);
+label.addEventListener("mouseover", hoverToggleText)
+label.addEventListener("mouseout", outToggleText)
+
+function activeToggle(){
+  document.body.classList.toggle("light")
+  if(document.body.classList.contains("light")){
+    hovercolor = "hsl(230, 17%, 14%)"
+    outcolor = "hsl(228, 12%, 44%)"
+    out()
+    hover()
+  }else{
+    hovercolor = "hsl(0, 0%, 100%)"
+    outcolor = "hsl(228, 34%, 66%)"
+    out()
+    hover()
+  }
+}
+
+function hoverToggleText(){
+    toggleText.style.color = hovercolor
+}
+
+function outToggleText(){
+    toggleText.style.color = outcolor
+}
